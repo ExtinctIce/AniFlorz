d1ece1c1487064284933a860bbe429d02a143969
Extinct <slarkbs@gmail.com>
2025-04-28 02:42:51 +0300

    Update AnimeCommunity.tsx

--- src/features/MainPage/CommunityPage/AnimeCommunity.tsx
+++ src/features/MainPage/CommunityPage/AnimeCommunity.tsx
@@ -54,7 +54,7 @@ export const AnimeCommunity = observer(() => {
               variants={cardAnimation}
               className="group bg-neutral-900 rounded-xl overflow-hidden cursor-pointer hover:bg-neutral-800 transition-colors duration-200"
             >
-              {/* Изображение с оверлеем */}
+      
               <div className="relative aspect-video">
                 <img
                   src={`https://static-libria.weekstorm.one${item.image.preview}`}
@@ -62,7 +62,7 @@ export const AnimeCommunity = observer(() => {
                   className="w-full h-full object-cover"
                 />
 
-                {/* Кнопка воспроизведения */}
+            
                 <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-200 bg-black/40">
                   <button
                     onClick={() => window.open(item.url, "_blank")}
@@ -74,7 +74,7 @@ export const AnimeCommunity = observer(() => {
                 </div>
               </div>
 
-              {/* Информация */}
+         
               <div className="p-4">
                 <h3 className="text-white font-medium mb-2 line-clamp-2">
                   {item.title}
